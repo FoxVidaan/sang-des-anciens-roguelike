16ca641f9c0fe3d01ad43a0950b861b26578b364
FerreiraAndgel <a.dejesus@kinomap.net>
2026-05-20 15:44:25 +0200

    Deploy GitHub Pages

--- src/app/core/data/scenarios.data.ts
+++
@@ -1,9 +0,0 @@
-import { ScenarioDefinition } from '../../models/game.models';
-import { ERRANT_DU_TEMPS_SCENARIO } from './scenarios/errant-du-temps.events.data';
-
-export const DEFAULT_SCENARIO_ID: string = ERRANT_DU_TEMPS_SCENARIO.id;
-
-export const SCENARIOS: readonly ScenarioDefinition[] = [ERRANT_DU_TEMPS_SCENARIO];
-
-export const findScenario = (scenarioId: string): ScenarioDefinition | null =>
-  SCENARIOS.find((scenario: ScenarioDefinition): boolean => scenario.id === scenarioId) ?? null;
